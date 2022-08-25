a32820ab002efd0850765aa424d4a6746237b68c
William Castillo <>
2022-08-25 03:49:23 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,11 @@
+var firstNumber = 2
+var secondNumber = 3
+
+var totalSum = firstNumber + secondNumber
+
+firstNumber = firstNumber + 1
+secondNumber = secondNumber + 1
+
+totalSum = firstNumber + secondNumber
+
+print("totalSum = \(totalSum)")
